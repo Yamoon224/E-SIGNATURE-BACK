b9257a4896dc080166cfc69a45bf6c55ac7916a4
Yamoon <160267741+Yamoon224@users.noreply.github.com>
2025-06-25 15:52:49 +0000

    Update WebConfig.java

--- src/main/java/com/signature/config/WebConfig.java
+++ src/main/java/com/signature/config/WebConfig.java
@@ -11,15 +11,22 @@ import java.nio.file.Paths;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    // Configuration CORS pour autoriser Vercel à accéder aux fichiers statiques
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/assets/**")
+                .allowedOrigins("https://e-signature-seven.vercel.app")
+                .allowedMethods("GET")
+                .allowedHeaders("*");
+    }
+
+    // Configuration pour servir le dossier /assets en tant que ressources statiques
     @Override
     public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
         Path uploadDir = Paths.get("assets");
         String uploadPath = uploadDir.toFile().getAbsolutePath();
 
         registry.addResourceHandler("/assets/**")
-                .allowedOrigins("https://e-signature-seven.vercel.app")
-                .allowedMethods("GET")
-                .allowedHeaders("*")
                 .addResourceLocations("file:" + uploadPath + "/");
     }
 }
